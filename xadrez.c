f416b21164d9ac3a73717a0cd2e68ca8c2f0a6da
izador4 <aizadora2002@gmail.com>
2025-06-03 17:00:28 -0300

    Adiciona o movimento do cavalo utilizando loops aninhados.

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 int main()
 {
-    int bispo;
-    int torre = 0;
-    int rainha = 0;
+    int movimentoBispo;
+    int movimentoTorre = 0;
+    int movimentoRainha = 0;
+    int movimentoCavalo = 1;
 
     // Move a torre cinco casas para a direita
-    while (torre < 5)
+    while (movimentoTorre < 5)
     {
         printf("Torre : Direita\n");
-        torre++;
+        movimentoTorre++;
     }
     // Move a rainha oito casas para esquerda
     do
     {
         printf("Rainha : Esquerda\n");
-        rainha++;
+        movimentoRainha++;
 
-    } while (rainha < 8);
+    } while (movimentoRainha < 8);
 
     // Move o bispo 5 casas para a diagonal(para cima e à direita)
-    for (bispo = 0; bispo < 5; bispo++)
+    for (movimentoBispo = 0; movimentoBispo < 5; movimentoBispo++)
     {
         printf("Bispo : Para cima e à direita\n");
     }
 
+    // Move o cavalo duas casas baixo e uma à esquerda
+
+    while (movimentoCavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cavalo : Para baixo\n");
+        }
+        printf("Cavalo : Esquerda\n");
+    }
     return 0;
 }
